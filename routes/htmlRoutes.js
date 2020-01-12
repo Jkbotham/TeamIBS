f6feee0e19611160d963b61e2792de6b04a21777
James Botham <jamesbotham@gmail.com>
2020-01-12 00:46:37 -0600

    added comments and cleaned code in html routes

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,9 +1,8 @@
 const db = require("../models");
 
-
 module.exports = (app, passport) => {
 
-	// Load index page
+	// Load index page with all ideas in database
 	app.get("/", (req, res) => {
 
 		db.Idea.findAll({
@@ -16,9 +15,8 @@ module.exports = (app, passport) => {
 			group: ['Idea.id'],
 			order: [['id', 'DESC']]
 		})
-			// Promise.all([id, commentCount])
 			.then(results => {
-				console.log(JSON.stringify(results))
+				// console.log(JSON.stringify(results))
 				res.render("index", { idea: results, user: req.user })
 			})
 			.catch(err => {
@@ -26,7 +24,7 @@ module.exports = (app, passport) => {
 			})
 	});
 
-	// load idea page
+	// Gets a specific idea and comments from database and returns Idea page
 	app.get("/idea/:id", (req, res) => {
 		const reqId = req.params.id
 
@@ -47,28 +45,27 @@ module.exports = (app, passport) => {
 				}
 			})
 			Promise.all([idea, comments, user])
-			.then(results => {
+				.then(results => {
 
-				res.render("idea", { idea: results[0], comments: results[1], user: results[2] });
-			})
-			.catch(err => {
-				console.log(err)
-			})
-	
-			}
-			else {
-				Promise.all([idea, comments])
-					.then(results => {
-						// console.log(JSON.stringify(results));
-						// console.log(JSON.stringify(results[1]));
-						res.render("idea", { idea: results[0], comments: results[1] });
-					})
-					.catch(err => {
-						console.log(err)
-					})
-			}
-		});
+					res.render("idea", { idea: results[0], comments: results[1], user: results[2] });
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}else {
+			Promise.all([idea, comments])
+				.then(results => {
+					// console.log(JSON.stringify(results));
+					// console.log(JSON.stringify(results[1]));
+					res.render("idea", { idea: results[0], comments: results[1] });
+				})
+				.catch(err => {
+					console.log(err)
+				})
+		}
+	});
 
+	// Sends client submit page // If user is logged in query database and return users unique id
 	app.get("/submit", (req, res) => {
 		if (req.user) {
 			db.User.findOne({
@@ -76,9 +73,9 @@ module.exports = (app, passport) => {
 					user_id: req.user
 				}
 			}).then((results) => {
-				console.log("Test: "+JSON.stringify(results))
+				// console.log("Test: " + JSON.stringify(results))
 				res.render("submit", { user: results })
-	
+
 			})
 		}
 		else {
@@ -86,60 +83,54 @@ module.exports = (app, passport) => {
 		}
 	})
 
-	app.get("/html")
-
-
-
+	//Jump off point for Facebook Auth landing page
 	app.get("/login", (req, res) => {
 		res.render('login', { user: req.user })
 	});
 
+	//Querys database returns the logged ins users ideas and comments -- Returns profle page.
 	app.get("/profile", ensureAuthenticated, (req, res) => {
 		db.User.findOne({
 			where: {
 				user_id: req.user
 			}
 		})
-		.then((results) => {
-			console.log(results)
+			.then((results) => {
+				console.log(results)
 
-		 let ideas = db.Idea.findAll({
-			where: { 
-				UserId: results.id
-			},
-			attributes: {
-				include: [[db.Sequelize.fn("count", db.Sequelize.col("Comments.id")), "commentCount"]]
-			},
-			include: [{
-				model: db.Comment, attributes: []
-			}],
-			group: ['Idea.id'],
-			order: [['id', 'DESC']]
-		})
+				let ideas = db.Idea.findAll({
+					where: {
+						UserId: results.id
+					},
+					attributes: {
+						include: [[db.Sequelize.fn("count", db.Sequelize.col("Comments.id")), "commentCount"]]
+					},
+					include: [{
+						model: db.Comment, attributes: []
+					}],
+					group: ['Idea.id'],
+					order: [['id', 'DESC']]
+				})
 
-		 let comments = db.Comment.findAll({
-			where: {
-				UserId: results.id
-			}
-		})
-		
-		
-		Promise.all([ideas,comments])
-			.then((Result) => {
-			console.log("Test:  ", Result, results)
-				res.render("profile", { user: results, ideas: Result[0], comments: Result[1] })
-		})
-
-
-		})
-		
-	
+				let comments = db.Comment.findAll({
+					where: {
+						UserId: results.id
+					}
+				})
+				Promise.all([ideas, comments])
+					.then((Result) => {
+						console.log("Test:  ", Result, results)
+						res.render("profile", { user: results, ideas: Result[0], comments: Result[1] })
+					})
+			})
 	})
 
+	//Redirects client ot facebook to login with variables to return
 	app.get("/auth/facebook", passport.authenticate("facebook", {
 		scope: ["public_profile"]
 	}))
 
+	//Switch used ot direct a successful login to the userpage or back to login if the auth was unsuccessful
 	app.get("/auth/facebook/callback", passport.authenticate("facebook", {
 		successRedirect: "/profile",
 		failureRedirect: "/login"
@@ -150,30 +141,25 @@ module.exports = (app, passport) => {
 		}
 	);
 
+	//Logs the client out and ends session
 	app.get("/logout", (req, res) => {
 		req.logout()
 		res.redirect("/")
 	});
+
 	// Render 404 page for any unmatched routes
 	app.get("*", (req, res) => {
 		res.render("404");
 	});
 
+	//verifies if client is currently logged if and redirecting to login page if false
 	function ensureAuthenticated(req, res, next) {
 		if (req.isAuthenticated()) { return next(); }
 		res.redirect('/login');
 	}
 
+	//Returning client with top ideas
 	app.get("/best", (req, res) => {
-		res.render("index", {idea: results, comments: results})
-	})
-
-
+		res.render("index", { idea: results, comments: results })
+	});
 }
-
-
-// Count of comments from one user
-// Count of ideas from one user
-
-
-
